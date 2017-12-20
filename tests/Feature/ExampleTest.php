7994515f44bbd095fc84726cb7a52518efbf7140
Nafies Luthfi <nafiesl@gmail.com>
2017-12-20 14:03:44 +0800

    Update BaseTestCase, simple-crud config, ExampleTest, and phpunit.xml

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -16,8 +16,6 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $this->visit('/');
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,20 @@
 
 namespace Tests;
 
-use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use App\User;
+use Laravel\BrowserKitTesting\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
+    protected $baseUrl = 'http://localhost';
+
     use CreatesApplication;
+
+    protected function loginAsUser()
+    {
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+
+        return $user;
+    }
 }
